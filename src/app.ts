803b7c77e2ee4cd5b1698fba58cf258fe44aacba
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-09-30 22:26:22 +0530

    adding student router

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-import express from 'express'
-import teacherRouter from './routes/teacherRoutes'
-const port = process.env.PORT || 3000
-const app = express()
-app.use(express.json())
+import express from "express";
+import teacherRouter from "./routes/teacherRoutes";
+import studentRouter from "./routes/studentRoutes";
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
 
-app.get('/api/v1/', (req, res) => {
-  res.send("hi there")
-})
-app.use("/api/v1/teacher", teacherRouter)
+app.get("/api/v1/", (req, res) => {
+  res.send("hi there");
+});
+app.use("/api/v1/teacher", teacherRouter);
+app.use("/api/v1/student", studentRouter);
 
 app.listen(3000, () => {
-  console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
