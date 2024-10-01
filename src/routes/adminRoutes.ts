7149ee279392b5f6fdc2c0a3f5cdbd25e2c6d311
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-01 22:32:53 +0530

    Admin router refector adding responce and fallback

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -2,7 +2,20 @@ import express from "express"
 import { AdminSignIn } from "../services/adminService";
 
 const adminRouter = express.Router();
-adminRouter.post("/signin", (req, res) => {
-  AdminSignIn(req, res)
+adminRouter.post("/login", async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  try {
+
+    const token = await AdminSignIn({ email, password })
+    console.log(token)
+    res.status(200).json({
+      token
+    })
+  } catch (err: any) {
+    res.status(401).json({
+      msg: err.message
+    })
+  }
 })
 export default adminRouter
