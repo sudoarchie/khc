f665bf5f6d0a2a991ade3e9fa175a2c99afffba3
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-01 22:32:12 +0530

    Adding app router

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
 import express from "express";
-import teacherRouter from "./routes/teacherRoutes";
-import studentRouter from "./routes/studentRoutes";
-import adminRouter from "./routes/adminRoutes";
+import mainRouter from "./router";
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-app.get("/api/v1/", (req, res) => {
-  res.send("hi there");
+app.get("/health", (_, res) => {
+  res.send("All things working fine")
 });
-app.use("/api/v1/teacher", teacherRouter);
-app.use("/api/v1/student", studentRouter);
-app.use("/api/v1/admin", adminRouter)
+
+app.use('/api/v1', mainRouter)
 app.listen(3000, () => {
   console.log(`http://localhost:${port}`);
 });
