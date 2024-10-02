26c2f8a386ee8d1415f2ee9955ba962f854be164
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-02 12:21:07 +0530

    main Router refector

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import mainRouter from "./router";
+import mainRouter from "./mainrouter";
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
