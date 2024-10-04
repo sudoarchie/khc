a82620d34e515fd12ee999a2fed091d9e2bf9afd
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-04 22:48:26 +0530

    Teacher Controller

--- src/controllers/teacherControllers.ts
+++ src/controllers/teacherControllers.ts
@@ -1,14 +1,41 @@
 import express from "express";
-import { TeacherSignIn, TeacherSignUp } from "../services/teacherService";
+import { TeacherLogin, TeacherSignUp } from "../services/teacherService";
 
 const teacherRouter = express.Router();
 
-teacherRouter.post("/signup", (req, res) => {
-  TeacherSignUp(req, res);
+teacherRouter.post("/signup", async (req, res) => {
+  const { name, firstName, secondName, thirdName, email, password } = req.body
+  try {
+
+    const response = await TeacherSignUp({ name, firstName, secondName, thirdName, email, password });
+    res.status(200).json({
+      msg: response.msg
+    })
+  } catch (err) {
+    res.status(400).json({
+      msg: err
+    })
+  }
 });
 
-teacherRouter.post("/signin", (req, res) => {
-  TeacherSignIn(req, res);
-});
+teacherRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+
+    const token = await TeacherLogin({ email, password });
+    res.status(200).json({
+      msg: "Login Successful!!",
+      token
+    })
+  } catch (err) {
+    res.status(403).json({
+      msg: err
+    })
+  }
+
+
+}
+
+);
 
 export default teacherRouter;
