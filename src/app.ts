040aafa5248339aad7d0a1fff46c667bf91a6ccf
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-05 16:00:07 +0530

    Setting cookie-parser cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import mainRouter from "./mainrouter";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(cookieParser())
 app.use(express.json());
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3001"
+}))
 
 app.get("/health", (_, res) => {
   res.send("All things working fine")
