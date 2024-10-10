65545f923444395f3b255316903b0d7339942c92
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-11 01:12:33 +0530

    Adding CurriculumRouter

--- src/mainrouter.ts
+++ src/mainrouter.ts
@@ -2,11 +2,13 @@ import express from "express"
 import teacherRouter from "./controllers/teacherControllers"
 import studentRouter from "./controllers/studentControllers"
 import adminRouter from "./controllers/adminControllers"
+import curriculumRouter from "./controllers/curriculumControllers"
 
 const mainRouter = express.Router()
 
 mainRouter.use('/teacher', teacherRouter)
 mainRouter.use('/student', studentRouter)
 mainRouter.use('/admin', adminRouter)
+mainRouter.use('/curriculum', curriculumRouter)
 
 export default mainRouter
