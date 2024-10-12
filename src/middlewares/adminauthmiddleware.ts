9d360addc334a6daf24fccb23a9a079663aae252
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-12 10:18:45 +0530

    Admin Auth Middleware

---
+++ src/middlewares/adminauthmiddleware.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+// Secret key (should be in an environment variable)
+const JWT_SECRET = process.env.JWT_SECRET || "MYsuperSECRETpassword";
+
+export function AuthAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any {
+  const token = req.cookies.admintoken;
+
+  if (!token) {
+    return res.status(401).json({
+      msg: "Cookies not found!!",
+    });
+  }
+
+  try {
+    // Verify the token with the secret key
+    const verified = jwt.verify(token, JWT_SECRET);
+
+    if (!verified) {
+      return res.status(401).json({
+        msg: "Invalid Cookie",
+      });
+    }
+
+    // Token is valid, proceed to the next middleware or route
+    next();
+  } catch (error) {
+    // If verification fails (e.g., invalid signature or expired token)
+    return res.status(403).json({
+      msg: "Failed to authenticate token",
+      error: error,
+    });
+  }
+}
