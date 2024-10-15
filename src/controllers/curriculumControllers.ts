7699058e0ef20ba24f63968fb5b6c15f49ace823
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-15 18:40:12 +0530

    [MULTER]: uploading curriculum logo and which is uploaded to s3 can be saved in db as link

--- src/controllers/curriculumControllers.ts
+++ src/controllers/curriculumControllers.ts
@@ -1,35 +1,50 @@
-import express from 'express'
-import { Get, Create } from '../services/curriculumService'
-import { AuthAdmin } from '../middlewares/adminauthmiddleware';
+import express from "express";
+import { Get, Create } from "../services/curriculumService";
+import { AuthAdmin } from "../middlewares/adminauthmiddleware";
+import { upload } from "../utils/uploadfile";
 
 const curriculumRouter = express.Router();
 
-curriculumRouter.post('/add', AuthAdmin, async (req, res) => {
-  const { name, description, logoUrl } = req.body
+curriculumRouter.post(
+  "/add",
+  AuthAdmin,
+  upload.single("file"),
+  async (req, res) => {
+    const { name, description } = req.body;
+    try {
+      if (!req.file) {
+        res.status(400).json({
+          msg: "No file uploaded",
+        });
+        return;
+      }
+      const file = req.file as Express.MulterS3.File;
+      const logoUrl = file.location;
+
+      const data = await Create({ name, description, logoUrl });
+      res.status(200).json({
+        msg: data.msg,
+        data: data.curriculum,
+      });
+    } catch (err) {
+      res.status(403).json({
+        msg: err instanceof Error ? err.message : "An unknown error occurred",
+      });
+    }
+  }
+);
+
+curriculumRouter.get("/data", async (req, res) => {
   try {
-    const data = await Create({ name, description, logoUrl })
+    const data = await Get();
     res.status(200).json({
-      msg: data.msg,
-      data: data.curriculum
-    })
+      data,
+    });
   } catch (err) {
     res.status(403).json({
-      msg: err instanceof Error ? err.message : 'An unknown error occurred'
-    })
+      msg: err instanceof Error ? err.message : "Something went wrong!!",
+    });
   }
-})
+});
 
-curriculumRouter.get('/data', async (req, res) => {
-  try {
-    const data = await Get()
-    res.status(200).json({
-      data
-    })
-  } catch (err) {
-    res.status(403).json({
-      msg: err instanceof Error ? err.message : 'Something went wrong!!'
-    })
-  }
-})
-
-export default curriculumRouter
+export default curriculumRouter;
