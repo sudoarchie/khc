7cb3d8358ba29699ac3de4376c49ce15e8d8f35e
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-16 15:29:41 +0530

    zod in admin controller for input validation

--- src/controllers/adminControllers.ts
+++ src/controllers/adminControllers.ts
@@ -1,18 +1,32 @@
 import express from "express"
 import { AdminSignIn } from "../services/adminService";
-
+import { z } from 'zod'
 const adminRouter = express.Router();
+const adminSchema = z.object({
+  email: z.string().email(`Invalid email formate`),
+  password: z.string().min(8, 'Password must contain at least 8 characters'),
+})
+
 adminRouter.post("/login", async (req, res) => {
   const email = req.body.email
   const password = req.body.password
   try {
+    const data = adminSchema.safeParse({ email, password })
+    if (!data.success) {
+      console.log(`Invalid Input`)
+      res.status(403).json({
+        msg: `Invalid Input`
+      })
+    } else {
 
-    const token = await AdminSignIn({ email, password })
-    console.log(token)
-    res.cookie("admintoken", token)
-    res.status(200).json({
-      msg: "Login Successful!!"
-    })
+      const token = await AdminSignIn({ email, password })
+      console.log(token)
+      res.cookie("admintoken", token)
+      res.status(200).json({
+        msg: "Login Successful!!"
+      })
+
+    }
   } catch (err: any) {
     res.status(401).json({
       msg: err.message
