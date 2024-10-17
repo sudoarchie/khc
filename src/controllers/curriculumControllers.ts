c3534488d80ef555552e452401c84cb4385f8eff
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-17 10:06:17 +0530

    zod in curriculum route

--- src/controllers/curriculumControllers.ts
+++ src/controllers/curriculumControllers.ts
@@ -2,9 +2,13 @@ import express from "express";
 import { Get, Create } from "../services/curriculumService";
 import { AuthAdmin } from "../middlewares/adminauthmiddleware";
 import { upload } from "../utils/uploadfile";
-
+import { z } from "zod";
 const curriculumRouter = express.Router();
 
+const addSchema = z.object({
+  name: z.string(),
+  description: z.string()
+})
 curriculumRouter.post(
   "/add",
   AuthAdmin,
@@ -12,20 +16,28 @@ curriculumRouter.post(
   async (req, res) => {
     const { name, description } = req.body;
     try {
-      if (!req.file) {
-        res.status(400).json({
-          msg: "No file uploaded",
-        });
-        return;
-      }
-      const file = req.file as Express.MulterS3.File;
-      const logoUrl = file.location;
+      const validateSchema = addSchema.safeParse({ name, description })
+      if (!validateSchema.success) {
+        res.status(403).json({
+          msg: 'Invalid Inputs!!'
+        })
+      } else {
+        if (!req.file) {
+          res.status(400).json({
+            msg: "No file uploaded",
+          });
+          return;
+        }
 
-      const data = await Create({ name, description, logoUrl });
-      res.status(200).json({
-        msg: data.msg,
-        data: data.curriculum,
-      });
+        const file = req.file as Express.MulterS3.File;
+        const logoUrl = file.location;
+
+        const data = await Create({ name, description, logoUrl });
+        res.status(200).json({
+          msg: data.msg,
+          data: data.curriculum,
+        });
+      }
     } catch (err) {
       res.status(403).json({
         msg: err instanceof Error ? err.message : "An unknown error occurred",
