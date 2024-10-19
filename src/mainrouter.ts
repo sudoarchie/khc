67d0c3f736349caff87d53f2695050d372444d9f
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-19 17:51:15 +0530

    adding grade route

--- src/mainrouter.ts
+++ src/mainrouter.ts
@@ -1,17 +1,19 @@
-import express from "express"
-import teacherRouter from "./controllers/teacherControllers"
-import studentRouter from "./controllers/studentControllers"
-import adminRouter from "./controllers/adminControllers"
-import curriculumRouter from "./controllers/curriculumControllers"
-import subjectRouter from "./controllers/subjectControllers"
-import AssignmentRouter from "./controllers/assignmentControllers"
+import express from "express";
+import teacherRouter from "./controllers/teacherControllers";
+import studentRouter from "./controllers/studentControllers";
+import adminRouter from "./controllers/adminControllers";
+import curriculumRouter from "./controllers/curriculumControllers";
+import subjectRouter from "./controllers/subjectControllers";
+import AssignmentRouter from "./controllers/assignmentControllers";
+import gradeRouter from "./controllers/gradeControllers";
 
-const mainRouter = express.Router()
+const mainRouter = express.Router();
 
-mainRouter.use('/teacher', teacherRouter)
-mainRouter.use('/student', studentRouter)
-mainRouter.use('/admin', adminRouter)
-mainRouter.use('/curriculum', curriculumRouter)
-mainRouter.use('/subject', subjectRouter)
-mainRouter.use('/assignment', AssignmentRouter)
-export default mainRouter
+mainRouter.use("/teacher", teacherRouter);
+mainRouter.use("/student", studentRouter);
+mainRouter.use("/admin", adminRouter);
+mainRouter.use("/curriculum", curriculumRouter);
+mainRouter.use("/subject", subjectRouter);
+mainRouter.use("/assignment", AssignmentRouter);
+mainRouter.use("/grade", gradeRouter);
+export default mainRouter;
