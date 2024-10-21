dbcbcb25ede3f50913f90f1224c524cff367fbc9
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-21 23:20:01 +0530

    submit assignment

--- src/services/assignmentSubmitService.ts
+++ src/services/assignmentSubmitService.ts
@@ -1,24 +1,41 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 interface Submit {
-  studentAssignmentId: string,
-  SubmitUrl: string
+  studentAssignmentId: string;
+  SubmitUrl: string;
 }
 async function SubmitAssignment({ studentAssignmentId, SubmitUrl }: Submit) {
   try {
     const data = await prisma.assignmentSubmit.create({
       data: {
         studentAssignmentId,
-        SubmitUrl
-      }
-    })
-    return data
+        SubmitUrl,
+      },
+    });
+    return data;
   } catch (err) {
-    throw err
+    throw err;
   }
-
 }
 
-export { SubmitAssignment }
+async function GetSubmittedAssignment({ teacherId }: { teacherId: string }) {
+  try {
+    const data = await prisma.assignmentSubmit.findMany({
+      where: {
+        studentassignment: {
+          teacherId: teacherId,
+        },
+      },
+    });
+    return {
+      data,
+    };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+export { SubmitAssignment, GetSubmittedAssignment };
